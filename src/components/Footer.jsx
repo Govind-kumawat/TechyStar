6eeccc21b1961fb086e81338be9e450434d4d951
Govind-kumawat <80705927+Govind-kumawat@users.noreply.github.com>
2023-05-16 15:19:40 +0530

    first changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>TechyStar</h1>
+                <p>@all rights reserved</p>
+            </div>
+
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+
+                    <a href="https://www.youtube.com/channel/UCA625xx1Y42owlo9b_sXnaQ" target={"blank"} >Youtube</a>
+                    <a href="https://www.instagram.com/govind_kumawat26/" target={"blank"} >Instagram</a>
+                    <a href="https://github.com/Govind-kumawat" target={"blank"} >Github</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
